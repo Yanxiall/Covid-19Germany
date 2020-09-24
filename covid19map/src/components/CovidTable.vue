159aa2c3988685d7e2b8b3a729171e897aefa342
Yanxiall <yanxia_he@126.com>
2020-09-24 15:03:27 +0200

    debug deploy

--- covid19map/src/components/CovidTable.vue
+++ covid19map/src/components/CovidTable.vue
@@ -1,8 +1,7 @@
     <template>
     <div>
       <el-table
-        :data="tableData"
-       
+        :data="tableData"       
         :cell-style="cellStyle"
         :header-cell-style="headCellStyle"
         row-key="name"
